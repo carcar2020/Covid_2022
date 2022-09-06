053c3497d8b23912d9eafa44e712384b1534af81
carcar2020 <carlitos.ronaldo11@hotmail.com>
2022-09-06 16:24:55 -0400

    lineplot

--- Covid_2022.py
+++ Covid_2022.py
@@ -107,7 +107,11 @@ if total_covid19_cases and total_covid19_deaths != 0:
     
     #Total Covid-19 Cases by Month [Line Chart]
     fig_month_total_lineplot = px.line(totalcovid19_cases_by_month, x = totalcovid19_cases_by_month.index, y= 'Total Cases', title = 'Covid Cases Trend')
+    
+    fig_month_total.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False) 
     fig_month_total_lineplot.update_layout( plot_bgcolor = "rgb(205, 97, 85)", yaxis = (dict(showgrid=False)), xaxis = dict(tickmode="linear") )
+    
+    
     
     # Covid Deaths by month [Barchart]
    Deaths_by_month = df_selection.groupby(by='Month').sum()[['Total Deaths']].sort_values(by='Month')
